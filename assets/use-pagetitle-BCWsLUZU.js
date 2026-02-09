d74e40124f6fc4a8b221777e149e2480973a1da6
Chayanths312 <Chayanths312@users.noreply.github.com>
2026-02-09 19:44:44 +0000

    [ci] deploy site 251b4ea6f4746fdfaeea4fcc59d503f84cb44d6c

---
+++ assets/use-pagetitle-BCWsLUZU.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-CijCaBNw.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
--- assets/use-pagetitle-CcRbaqBf.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-ACnd9u82.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
